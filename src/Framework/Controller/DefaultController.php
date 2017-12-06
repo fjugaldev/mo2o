cf167d89412696c7bb050b19db4ebdff8bbf4484
Ayoze Hernández <ayozehp@hotmail.com>
2017-12-06 20:02:45 +0100

    Llamada desde el controlador al servicio Runtasty para obtener las recetas

--- src/Framework/Controller/DefaultController.php
+++ src/Framework/Controller/DefaultController.php
@@ -6,13 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Domain\Runtasty;
 
 class DefaultController extends Controller {
     /**
      * @Route("/", name="home")
      */
-    public function home (Request $request): JsonResponse{
-        $response = new JsonResponse(array());
+    public function home (Request $request, Runtasty $runtasty): JsonResponse{
+        $response = new JsonResponse($runtasty->getReceip());
         return $response;
     }
 }
