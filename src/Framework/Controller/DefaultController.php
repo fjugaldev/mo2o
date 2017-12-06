0eb95059b58dc19258c43568fce0d58778de792a
Ayoze Hernández <ayozehp@hotmail.com>
2017-12-07 00:20:38 +0100

    Cambio en url de API y paso de los términos de búsqueda

--- src/Framework/Controller/DefaultController.php
+++ src/Framework/Controller/DefaultController.php
@@ -10,10 +10,11 @@ use App\Domain\Runtasty;
 
 class DefaultController extends Controller {
     /**
-     * @Route("/{terms}", name="home", defaults={"terms"=""}, methods={"GET"})
+     * @Route("/api/receips", name="receips", methods={"GET"})
      */
-    public function home (string $terms, Request $request, Runtasty $runtasty): JsonResponse{
+    public function receips (Request $request, Runtasty $runtasty): JsonResponse{
         $page = $request->query->get('page', 1);
+        $terms = $request->query->get('terms', 1);
         $response = new JsonResponse($runtasty->getReceip($page, $terms));
         return $response;
     }
